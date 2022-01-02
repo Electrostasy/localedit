d96190032a3892e2c5fcd46eeb80cda15cbf067a
Juozas Rimantas <juozasr@wix.com>
2022-01-02 15:41:11 +0200

    Fix bug: initialize pointer

--- src/Components/MessageBox.cpp
+++ src/Components/MessageBox.cpp
@@ -1,7 +1,7 @@
 #include "MessageBox.h"
 
 QMessageBox *MessageBox::UnsavedChangesBox(const QString windowTitle) {
-	QMessageBox *unsavedChangesBox;
+	QMessageBox *unsavedChangesBox = new QMessageBox();
 	unsavedChangesBox->setWindowTitle(windowTitle);
 	unsavedChangesBox->setText("You have unsaved changes.");
 	unsavedChangesBox->setStandardButtons(QMessageBox::Save | QMessageBox::Ignore | QMessageBox::Cancel);
